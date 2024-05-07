e269a9557aa0109c4e21b488a1fd2f979c2f871c
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-06 21:02:18 -0300

    card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule, NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, NgFor,NgIf, NgSwitch, NgSwitchCase],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'card';
+  imagenList: { url: string, name: string , category: string}[] = [
+    { url: "./assets/child.jpg", name:"Niña jugando", category: "Categoría: Niño" },
+    { url: "./assets/ai-generated.jpg", name:"Miradas" , category: "Categoría: Adultos"},
+    { url: "./assets/couple.jpg", name:"Pareja de Ancianos", category: "Categoría: Adultos"},
+    { url: "./assets/crayons.jpg", name:"Colores", category: "Categoría Motricidad: Fina"},
+  ];
+
 }
